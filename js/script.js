0000eb671878b41cf0552bc8a9ffc3acfb6de84b
Sawa1Savka <133581993+Sawa1Savka@users.noreply.github.com>
2024-02-13 21:55:36 +0300

    FlowerShop

---
+++ js/script.js
@@ -0,0 +1,34 @@
+﻿var sliderIndex = 1;
+showSlides(sliderIndex);
+
+function plusSlides(n) {
+  showSlides(sliderIndex += n);
+}
+
+function currentSlides(n) {
+  showSlides(sliderIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    sliderIndex = 1;
+  }
+
+  if (n < 1) {
+    sliderIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[sliderIndex - 1].style.display = "block";
+  dots[sliderIndex - 1].className += " active";
+}
